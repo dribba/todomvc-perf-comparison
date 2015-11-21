027362c861e84380191993edf9dd23bc1a94b2a8
Yosuke Torii <jinjorweb@gmail.com>
2015-11-21 23:24:19 +0900

    update elm

--- resources/tests.js
+++ resources/tests.js
@@ -222,11 +222,47 @@ Suites.push({
         })
     ]
 });
+Suites.push({
+    name: 'Elm 0.15.1',
+    url: 'todomvc/elm-0.15.1/index.html',
+    version: '0.15.1 + virtual-dom ?',
+    prepare: function (runner, contentWindow, contentDocument) {
+        return runner.waitForElement('#new-todo').then(function (element) {
+            element.focus();
+            return element;
+        });
+    },
+    tests: [
+        new BenchmarkTestStep('Adding' + numberOfItemsToAdd + 'Items', function (newTodo, contentWindow, contentDocument) {
+            for (var i = 0; i < numberOfItemsToAdd; i++) {
+                var inputEvent = document.createEvent('Event');
+                inputEvent.initEvent('input', true, true);
+                newTodo.value = 'Something to do ' + i;
+                newTodo.dispatchEvent(inputEvent);
+
+                var keydownEvent = document.createEvent('Event');
+                keydownEvent.initEvent('keydown', true, true);
+                keydownEvent.keyCode = 13; // VK_ENTER
+                newTodo.dispatchEvent(keydownEvent);
+            }
+        }),
+        new BenchmarkTestStep('CompletingAllItems', function (newTodo, contentWindow, contentDocument) {
+            var checkboxes = contentDocument.querySelectorAll('.toggle');
+            for (var i = 0; i < checkboxes.length; i++)
+                checkboxes[i].click();
+        }),
+        new BenchmarkTestStep('DeletingAllItems', function (newTodo, contentWindow, contentDocument) {
+            var deleteButtons = contentDocument.querySelectorAll('.destroy');
+            for (var i = 0; i < deleteButtons.length; i++)
+                deleteButtons[i].click();
+        })
+    ]
+});
 
 Suites.push({
-    name: 'Elm',
-    url: 'todomvc/elm/index.html',
-    version: '0.12.3 + virtual-dom 0.8',
+    name: 'Elm 0.16',
+    url: 'todomvc/elm-0.16/index.html',
+    version: '0.16 + virtual-dom ?',
     prepare: function (runner, contentWindow, contentDocument) {
         return runner.waitForElement('#new-todo').then(function (element) {
             element.focus();
